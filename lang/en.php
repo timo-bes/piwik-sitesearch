ee5a59a5183258c3fb98aef0502ead8fca1a3d92
Timo Besenreuther <timo.besenreuther@gmail.com>
2010-07-17 09:51:08 +0200

    initial commit: example plugin copied

---
+++ lang/en.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Piwik - Open source web analytics
+ * 
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id: en.php 1816 2010-01-29 21:27:47Z vipsoft $
+ * 
+ * @category Piwik_Plugins
+ * @package Piwik_ExamplePlugin
+ */
+
+$translations = array(
+	'ExamplePlugin_PluginDescription' => 'Example Plugin: This plugin shows how to create a very simple plugin, that exports two widgets in the Dashboard.',
+	'ExamplePlugin_exampleWidgets' => 'Example Widgets',
+	'ExamplePlugin_exampleWidget' => 'Example widget',
+	'ExamplePlugin_blogPiwikRss' => 'Blog Piwik RSS',
+	'ExamplePlugin_photostreamMatt' => 'Matt\'s Photos',
+);
